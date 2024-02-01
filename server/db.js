2ead849e448a02dc8561a3a32b46fe6ec1093d59
ericpkatz <ericpkatz@gmail.com>
2024-02-01 14:25:17 -0500

    set up tables and seeded users and skills

---
+++ server/db.js
@@ -0,0 +1,61 @@
+const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_talent_agency_db');
+const uuid = require('uuid');
+const bcrypt = require('bcrypt');
+
+
+const createTables = async()=> {
+  const SQL = `
+    DROP TABLE IF EXISTS user_skills;
+    DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS skills;
+    CREATE TABLE users(
+      id UUID PRIMARY KEY,
+      username VARCHAR(100) UNIQUE NOT NULL,
+      password VARCHAR(255)
+    );
+    CREATE TABLE skills(
+      id UUID PRIMARY KEY,
+      name VARCHAR(100) UNIQUE NOT NULL
+    );
+    CREATE TABLE user_skills(
+      id UUID PRIMARY KEY,
+      skill_id UUID REFERENCES skills(id) NOT NULL,
+      user_id UUID REFERENCES users(id) NOT NULL,
+      CONSTRAINT unique_skill_user UNIQUE (skill_id, user_id)
+    );
+  `;
+  await client.query(SQL);
+};
+
+const createUser = async({ username, password })=> {
+  const SQL = `
+    INSERT INTO users(id, username, password) VALUES($1, $2, $3) RETURNING *
+  `;
+  const response = await client.query(SQL, [uuid.v4(), username, await bcrypt.hash(password, 5)]);
+  return response.rows[0];
+}
+
+const createSkill = async({ name })=> {
+  const SQL = `
+    INSERT INTO skills(id, name) VALUES($1, $2) RETURNING *
+  `;
+  const response = await client.query(SQL, [uuid.v4(), name]);
+  return response.rows[0];
+}
+
+const fetchUsers = async()=> {
+  const SQL = `
+    SELECT * FROM users;
+  `;
+  const response = await client.query(SQL);
+  return response.rows;
+}
+
+module.exports = {
+  client,
+  createTables,
+  createUser,
+  createSkill,
+  fetchUsers
+};
--- server/index.js
+++ server/index.js
@@ -1,1 +1,28 @@
-console.log('hello world');
+const {
+  client,
+  createTables,
+  createUser,
+  createSkill,
+  fetchUsers
+} = require('./db');
+
+
+const init = async()=> {
+  await client.connect();
+  console.log('connected to database');
+  await createTables();
+  console.log('tables created');
+  const [moe, lucy, ethyl, singing, dancing, juggling, plateSpinning] = await Promise.all([
+    createUser({ username: 'moe', password: 's3cr3t' }),
+    createUser({ username: 'lucy', password: 's3cr3t!!' }),
+    createUser({ username: 'ethyl', password: 'shhh' }),
+    createSkill({ name: 'singing'}),
+    createSkill({ name: 'dancing'}),
+    createSkill({ name: 'juggling'}),
+    createSkill({ name: 'plate spinning'}),
+  ]);
+  const users = await fetchUsers();
+  console.log(users);
+};
+
+init();
