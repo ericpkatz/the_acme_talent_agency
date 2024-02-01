2c349c9afa1b8e1fc0d593b2020fab0a3cbda469
ericpkatz <ericpkatz@gmail.com>
2024-02-01 14:50:04 -0500

    setup restful api

--- server/index.js
+++ server/index.js
@@ -10,6 +10,56 @@ const {
   userUserSkill,
   deleteUserSkill
 } = require('./db');
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get('/api/users', async(req, res, next)=> {
+  try {
+    res.send(await fetchUsers());
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.get('/api/skills', async(req, res, next)=> {
+  try {
+    res.send(await fetchSkills());
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.get('/api/users/:id/userSkills', async(req, res, next)=> {
+  try {
+    res.send(await fetchUserSkills(req.params.id));
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.post('/api/users/:id/userSkills', async(req, res, next)=> {
+  try {
+    res.status(201).send(await createUserSkill({ user_id: req.params.id, skill_id: req.body.skill_id}));
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.delete('/api/users/:userId/userSkills/:id', async(req, res, next)=> {
+  try {
+    await deleteUserSkill(req.params.id);
+    res.sendStatus(204);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
 
 
 const init = async()=> {
@@ -42,6 +92,13 @@ const init = async()=> {
   console.log(await fetchUserSkills(moe.id));
   await deleteUserSkill(userSkills[0].id);
   console.log(await fetchUserSkills(moe.id));
+
+  console.log(`CURL -X POST localhost:3000/api/users/${lucy.id}/userSkills -d '{"skill_id":"${plateSpinning.id}"}' -H 'Content-Type:application/json'`);
+
+  console.log(`CURL -X DELETE localhost:3000/api/users/${moe.id}/userSkills/${userSkills[1].id}`);
+  
+  const port = process.env.PORT || 3000;
+  app.listen(port, ()=> console.log(`listening on port ${port}`));
 };
 
 init();
